95dee8f865fc981363c76d0719f2a3e616511f9c
hero2bash <zero.bash@gmail.com>
2018-05-03 11:23:04 +0800

    Remove user route (unused)

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var monk = require('monk');
 var db = monk('localhost:27017/PCDialer');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -54,7 +53,6 @@ app.use(function(req,res,next){
 });
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
